b23b7bffe973b1eeabdc05fb49353bed4a69e9ae
haw230 <hancheng.wu22@gmail.com>
2019-09-07 01:43:01 -0400

    Revert "added api request"
    This reverts commit 82020da4294e8d7eedebd18ebf7cc6703c31fc66.

--- index.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-const getGoogleCloud = async () => (
-    await axios.get('https://jsonplaceholder.typicode.com/todos/1')
-);
-
-getGoogleCloud().then(data => console.log(data.data));
