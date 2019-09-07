406157b434f6151faef0574df947292a53c8ec3d
Chris Tan <chrisinquires@gmail.com>
2019-09-07 02:06:55 -0400

    push chris changes

--- index.js
+++ index.js
@@ -23,3 +23,10 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log("Server started on port " + PORT);
 });
+const axios = require('axios');
+
+const getGoogleCloud = async () => (
+    await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+);
+
+getGoogleCloud().then(data => console.log(data.data));
